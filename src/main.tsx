7eb4e6c714d5af2f6cf2a1703e5fcb62b87f1778
Salarnto <salarnto.collab@gmail.com>
2025-04-01 14:09:57 +0330

    fix(routing): move routing from main.tsx to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Routes, Route } from "react-router";
-import Cart from "./components/Cart.tsx";
-import { CartProvider } from "./contexts/CartContext.tsx";
-import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toaster position="top-right" />
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<App />} />
-            <Route path="cart" element={<Cart />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </StrictMode>
 );
